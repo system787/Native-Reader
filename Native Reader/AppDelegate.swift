c84b1298a3de41443e0c9b076caab1276c088c65
Vincent Hoang <vietnhoang7@gmail.com>
2018-08-07 15:03:38 -0700

    Initial Commit

---
+++ Native Reader/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Native Reader
+//
+//  Created by Vincent Hoang on 8/7/18.
+//  Copyright © 2018 Vincent Hoang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Native Reader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Native Reader
+//
+//  Created by Vincent Hoang on 8/7/18.
+//  Copyright © 2018 Vincent Hoang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
